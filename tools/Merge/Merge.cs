dfe74519cddc60bb713d25d81c4a40076c2e3d8d
Rcmcpe <rcmcpe-cn@outlook.com>
2022-01-12 18:43:56 +0800

    升级全自动化

---
+++ tools/Merge/Merge.cs
@@ -0,0 +1,77 @@
+﻿// 合并新版语言文件到模组
+// 作者 RcINS 2022-1-12
+
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+var translationsToCopy = new[] { "Original", "Chinese", "Chinesesimp" };
+
+DirectoryInfo? vanillaRoot = null;
+DirectoryInfo? modRoot = null;
+try
+{
+    vanillaRoot = new DirectoryInfo(args[0]);
+    modRoot = new DirectoryInfo(args[1]);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+    Console.WriteLine("合并新版语言文件到模组。");
+    Console.WriteLine("用法：Merge.exe <解包完毕的语言文件路径> <模组路径>");
+}
+
+static DirectoryInfo[] GetAddons(DirectoryInfo modDirectory)
+{
+    return modDirectory.EnumerateDirectories().SelectMany(d => d.EnumerateDirectories()).ToArray();
+}
+
+DirectoryInfo[] vanillaAddons = GetAddons(vanillaRoot!);
+DirectoryInfo[] modAddons = GetAddons(modRoot!);
+
+foreach (DirectoryInfo vanillaAddon in vanillaAddons)
+{
+    DirectoryInfo? modAddon = modAddons.FirstOrDefault(modAddon => modAddon.Name == vanillaAddon.Name);
+    if (modAddon == null)
+    {
+        Console.WriteLine($"需要翻译新 Addon: \"{vanillaAddon}\"");
+        continue;
+    }
+
+    string vanillaXmlPath = Path.Combine(vanillaAddon.FullName, "stringtable.xml");
+    XDocument vanillaXml = XDocument.Load(vanillaXmlPath);
+    XDocument modXml = XDocument.Load(Path.Combine(modAddon.FullName, "stringtable.xml"));
+    XElement[] vanillaKeys = vanillaXml.XPathSelectElements("Project/Package//Key").ToArray();
+    XElement[] modKeys = modXml.XPathSelectElements("Project/Package//Key").ToArray();
+    foreach (XElement modKey in modKeys)
+    {
+        XElement? vanillaKey = vanillaKeys.FirstOrDefault(vanillaKey => vanillaKey.Attribute("ID")!.Value ==
+                                                                        modKey.Attribute("ID")!.Value);
+        if (vanillaKey == null)
+        {
+            // 已经翻译了但是没出现在新版，说明已经被删除
+            Console.WriteLine($"键 \"{modKey.Attribute("ID")}\" 已被删除");
+            continue;
+        }
+
+        foreach (string translationToCopy in translationsToCopy)
+        {
+            XElement? modTranslation = modKey.Descendants(translationToCopy).FirstOrDefault();
+            if (modTranslation == null)
+            {
+                continue;
+            }
+
+            XElement? vanillaTranslation = vanillaKey.Descendants(translationToCopy).FirstOrDefault();
+            if (vanillaTranslation != null)
+            {
+                vanillaTranslation.ReplaceWith(modTranslation);
+            }
+            else
+            {
+                vanillaKey.Add(modTranslation);
+            }
+        }
+    }
+
+    vanillaXml.Save(vanillaXmlPath);
+}
--- tools/localization_extract.ps1
+++ tools/localization_extract.ps1
@@ -1,4 +1,6 @@
-$destination = 'D:/Dev/Arma3/Extract/'
+Set-StrictMode -Version Latest
+
+$extractDest = 'D:/Dev/Arma3/Extract/'
 $tools = 'D:\SteamLibrary\steamapps\common\Arma 3 Tools'
 
 # In Arma 3 root execute: ls -Directory | ? {((ls $_) | ? {$_.Name -eq 'Addons'}).Length -gt 0}
@@ -12,31 +14,45 @@ if (!$PSScriptRoot) {
     return
 }
 $xmlCleaner = Join-Path $PSScriptRoot 'XmlCleaner\XmlCleaner.exe'
+$merge = Join-Path $PSScriptRoot 'Merge\Merge.exe'
 
-if (Test-Path $destination) {
+if (Test-Path $extractDest) {
     Write-Warning 'Removing old files'
-    Remove-Item -Recurse $destination
+    Remove-Item -Recurse $extractDest
+    if (!$?) {
+        Write-Error 'Cannot remove old files. Check handles.'
+        return
+    }
 }
-$dest = New-Item -ItemType Directory $destination
 
-$combined = $core + ($expansions | ForEach-Object {Join-Path $_ 'Addons'})
-$pbos = Get-ChildItem $combined | Where-Object {$_.Extension -eq '.pbo' -and $_.Name -match 'language'}
+$combined = $core + ($expansions | ForEach-Object { Join-Path $_ 'Addons' })
+$pbos = Get-ChildItem $combined | Where-Object { $_.Extension -eq '.pbo' -and $_.Name -match 'language' }
 
 Write-Information 'Extracting...'
 $pbos | ForEach-Object {
-    & $bankRev -folder $dest -time $_.FullName
+    $addonDest = if ($_ -notmatch 'Dta') { Join-Path $dest 'Addons' } else { Join-Path $dest 'Dta' }
+    & $bankRev -folder $addonDest -time $_.FullName
 }
 
 Write-Information 'Debinarize Configs...'
-Get-ChildItem "$dest/**/*.bin" | ForEach-Object {
+Get-ChildItem "$extractDest/**/**/*.bin" | ForEach-Object {
     $cppDest = Join-Path $_.DirectoryName "$([IO.Path]::GetFileNameWithoutExtension($_)).cpp"
     & $binToCpp -txt -dst $cppDest $_.FullName
     Remove-Item $_
 }
 
 Write-Information 'Cleaning XML...'
-Get-ChildItem "$dest/**/*.xml" | ForEach-Object {
+Get-ChildItem "$extractDest/**/**/*.xml" | ForEach-Object {
     & $xmlCleaner $_.FullName
+}
+
+Write-Warning 'Before merging, make sure the working tree is clean, this will remove current sources!'
+$choice = Read-Host 'Merge to current branch? [y/n] (Default: n)'
+if ($choice -eq 'y') {
+    $src = Join-Path $PSScriptRoot '../src'
+    & $merge $extractDest $src
+    Remove-Item -Recurse (Get-ChildItem $extractDest | ForEach-Object {Join-Path $src $_.Name})
+    Copy-Item -Recurse "$extractDest/**" -Exclude '*.txt' $src
 }
 
 Write-Information 'Done'
